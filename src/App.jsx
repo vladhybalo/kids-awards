5e720f90bc5aeaea870177381d59da8afba16a97
unknown <vladhybalo@gmail.com>
2022-07-28 10:52:41 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+  return (
+    <div>
+      Root
+    </div>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { matchPath, matchRoutes } from 'react-router-dom'
+
+const Home = () => {
+
+
+	return <div>Home</div>
+}
+
+export default Home;
