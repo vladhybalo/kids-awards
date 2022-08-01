6bbbae8f8537af3ec817cce0501eb1a26b73bf18
andriiivaniuk <keed888888@gmail.com>
2022-08-01 18:23:45 +0300

    add spaces to import groups quick fix

--- src/store.jsx
+++ src/store.jsx
@@ -1,7 +1,9 @@
 import { legacy_createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+
 import reducers from "./ducks";
+
 import localStorageMiddleware from "./middlewares/localStorageMiddleware";
 
 const getInitialStore = () => {
